546b1df16eac392a2c102503e59dd2ad3228cfec
huch0 <heochiyeong@gmail.com>
2023-02-18 21:50:08 +0900

    0.11.0

--- scripts/get_posts.js
+++ scripts/get_posts.js
@@ -1,51 +1,78 @@
 
-const get_posts = async (searchType, searchTerm, numberOfResults, page) => {
-    //searchType = 'All', 'User', 'Title', 'Category'
-    console.log(searchType, searchTerm, numberOfResults, page);
+const get_posts = (searchType, searchTerm, numberOfResults, page) => {
+    return new Promise((resolve, reject) => {
+        //searchType = 'All', 'User', 'Title', 'Category'
+        console.log(searchType, searchTerm, numberOfResults, page);
 
-    if (!searchType || !searchTerm || !numberOfResults || !page) {
-        alert('검색 Form을 모두 채우세요');
-        return;
-    }
+        if (!searchType || !searchTerm || !numberOfResults || !page) {
+            alert('검색 Form을 모두 채우세요');
+            reject(new Error('검색 Form을 모두 채우세요'));
+        }
 
-    const query = `searchType=${searchType}&searchTerm=${searchTerm}&numberOfResults=${numberOfResults}&page=${page}`;
+        const query = `searchType=${searchType}&searchTerm=${searchTerm}&numberOfResults=${numberOfResults}&page=${page}`;
 
-    axios.get(`/post_db/search?${query}`)
-        .then(response => {
-            console.log(response.data);
-            const posts = response.data.posts;
-            console.log('posts', posts);
+        axios.get(`/post_db/search?${query}`)
+            .then(response => {
+                console.log(response.data);
+                const posts = response.data.posts;
+                console.log('posts', posts);
 
-            let cardsHtml = "";
-            const search_result = document.getElementById("search_result");
+                let cardsHtml = "";
 
-            for (let i = 0; i < posts.length; i++) {
-                const post = posts[i];
-                console.log('post', post);
-                cardsHtml += createDiv(post.path, post.thumbnail_url, post.Category2Id, post.title, post.description, post.UserId, format_date(post.createdAt), "2400");
-            }
-            search_result.innerHTML = cardsHtml;
-        })
-        .catch(error => {
-            console.error(error);
-            alert("Failed to retrieve posts. Please try again later.");
-        });
-
-}
-
+                for (let i = 0; i < posts.length; i++) {
+                    const post = posts[i];
+                    console.log('post', post);
+                    cardsHtml += createDiv(post.path, post.thumbnail_url, post.Category2Id, post.title, post.description, post.UserId, format_date(post.createdAt), "2400");
+                }
+                //console.log('cardsHtml : ', cardsHtml);
+                resolve(cardsHtml);
+            })
+            .catch(error => {
+                console.error(error);
+                alert("요청이 너무 빠릅니다. 잠시 후 다시 시도하세요.");
+                reject(error);
+            });
+    });
+};
 function createDiv(path, imgSrc, category_2, title, description, user, date, views) {
     return `
-        <div class="card p-0" style="width: 320px; height: 360px;">
+        <div class="card p-0" style="width: 20rem;">
             <a class="nav-link" href="/${path}">
-                <img src="${imgSrc}" class="card-img-top" alt="thumbnail" style="width: 320px; height: 180px;">
-                <div class="card-body" style="width: 320px; height: 180px;">
-                    <h6 class="card-subtitle mb-2 text-muted">${category_2}</h6>
+                <img src="${imgSrc}" class="card-img-top" alt="thumbnail" style="height: 180px;">
+                <div class="card-body">
+                    <p class="card-text mb-2 text-muted">${category_2}</p>
                     <h5 class="card-title">${title}</h5>
                     <p class="card-text">${description}</p>
-                    <h6 class="card-subtitle mb-2 text-muted">by ${user} - ${date}</h6>
-                    <h6 class="card-subtitle mb-2 text-muted">${views} views</h6>
+                    <p class="card-text mb-2 text-muted">by ${user} - ${date}</p>
+                    <p class="card-text mb-2 text-muted">${views} views</p>
                 </div>
             </a>
         </div>
     `;
 }
+/*
+`
+        <div class="card" style="width: 20rem;">
+            <a class="nav-link" href="/${path}">
+                <img src="${imgSrc}" class="card-img-top" alt="thumbnail">
+                <div class="card-body">
+                    <p class="card-text mb-2 text-muted">${category_2}</p>
+                    <h5 class="card-title">${title}</h5>
+                    <p class="card-text">${description}</p>
+                    <p class="card-text mb-2 text-muted">by ${user} - ${date}</p>
+                    <p class="card-text mb-2 text-muted">${views} views</p>
+                </div>
+            </a>
+        </div>
+    `;
+    */
+/*
+<div class="card" style="width: 20rem;">
+     <img src="http://localhost:3000/img/1676708324761.png" class="card-img-top" alt="thumbnail" style="height: 180px;">  
+     <div class="card-body">
+       <h5 class="card-title">Card title</h5>
+       <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+       <a href="#" class="btn btn-primary">Go somewhere</a>
+     </div>
+   </div>
+*/
--- scripts/home.js
+++ scripts/home.js
@@ -1,10 +1,31 @@
+
 window.onload = () => {
   if (new URL(location.href).searchParams.get('loginError')) {
     alert(new URL(location.href).searchParams.get('loginError'));
   }
 
+  const search_result = document.getElementById("search_result");
+  // Check if the post data is already stored in localStorage and is not expired
+  const post_data = localStorage.getItem('post_data');
+  const postDataExpiration = localStorage.getItem('post_data_expiration');
 
-  get_posts('All', 1, 4, 1);
+  if (post_data && postDataExpiration && Date.now() < postDataExpiration) {
+    const cardsHtml = JSON.parse(post_data);
+    search_result.innerHTML = cardsHtml;
+    console.log('localStorage에서 가져옴.');
+  } else {
+    // If the post data is not available or expired, fetch it from the server
+    get_posts('All', 1, 4, 1).then(cardsHtml => {
+      console.log('in home.js / posts :', cardsHtml);
+      // Save the post data in localStorage with an expiration time of 5 minutes
+      const expiration = Date.now() + (5 * 60 * 1000); // 5 minutes from now
+      localStorage.setItem('post_data', JSON.stringify(cardsHtml));
+      localStorage.setItem('post_data_expiration', expiration);
+      
+
+      search_result.innerHTML = cardsHtml;
+    });
+  }
 };
 
 function format_date(date_str) {
